68044666ed9b99225c8ed95aff3a6b4ff29f4375
Juan Corado <juancorado1@gmail.com>
2020-07-23 21:41:16 -0700

    First commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,48 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 
 void main() => runApp(
       MaterialApp(
-        home: null,
+        home: BallPage(),
       ),
     );
+
+class BallPage extends StatefulWidget {
+  @override
+  _BallPageState createState() => _BallPageState();
+}
+
+class _BallPageState extends State<BallPage> {
+  int randomResponse = 1;
+
+  void randomBallPage() {
+    setState(() {
+      randomResponse = Random().nextInt(5) + 1;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.blue[900],
+        centerTitle: true,
+        title: Text('Magic 8-ball'),
+      ),
+      body: SafeArea(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            FlatButton(
+              child: Image.asset('images/ball$randomResponse.png'),
+              onPressed: () {
+                randomBallPage();
+              },
+            ),
+          ],
+        ),
+      ),
+      backgroundColor: Colors.blue,
+    );
+  }
+}
